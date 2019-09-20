26090cdd3e7230f51ae3e00da74b01e4eb785a9e
Coded <coded@hrznstudio.com>
2019-09-20 18:46:46 +0100

    :tada: Initial commit

---
+++ src/main/java/com/hrznstudio/sandbox/example/ExampleMod.java
@@ -0,0 +1,16 @@
+package com.hrznstudio.sandbox.example;
+
+import com.hrznstudio.sandbox.api.SandboxAPI;
+import com.hrznstudio.sandbox.api.addon.Addon;
+
+public class ExampleMod implements Addon {
+    @Override
+    public void init(SandboxAPI api) {
+
+    }
+
+    @Override
+    public void register() {
+
+    }
+}
