4dead5f1ac479e23782290d2bba9327b95882fdd
Coded <coded@hrznstudio.com>
2019-09-21 23:11:23 +0100

    :wrench: Update Example

--- src/main/java/com/hrznstudio/sandbox/example/ExampleMod.java
+++ src/main/java/com/hrznstudio/sandbox/example/ExampleMod.java
@@ -6,7 +6,7 @@ import com.hrznstudio.sandbox.api.addon.Addon;
 public class ExampleMod implements Addon {
     @Override
     public void init(SandboxAPI api) {
-
+        api.getLog().info("Loading Example Addon");
     }
 
     @Override
